a1855e87be16abacd7e58e4edb4bb7c98a8d963c
njcx <njcx86@gmail.com>
2021-10-19 12:57:25 +0800

    add whitelist

--- blackwtredis/biptoredis.sh
+++ blackwtredis/biptoredis.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 
-dir=/opt/blocklist-ipsets
+dir=/data0/BlackIP/blackwtredis/blocklist-ipsets
+whitelist=/data0/BlackIP/blackwtredis/whitelist.txt
+
 redis=127.0.0.1
 redis_port=6381
-redis_pw=nBbIBrmoWHaLRJzu
- 
+redis_pw=123456
+mapfile  whitelistarr < $whitelist
+
+
 for file in $dir/*; do
 
 if [[ $file == *".ipset"* ]]
 then
-    echo $file | xargs cat| grep -v ^# | while read i;do      
-    redis-cli -h $redis -p $redis_port -a $redis_pw -c  set $i  $(basename ${file} .ipset); done
+    echo $file | xargs cat| grep -v ^# | while read i;do
+
+        if [[ ! "${whitelistarr[@]}"  =~ "$i" ]]; then
+          redis-cli -h $redis -p $redis_port -a $redis_pw -c  set $i  $(basename ${file} .ipset)
+        fi
+
+      done
+
 fi
 
 if [[ $file == *".netset"* ]]
 then
-    echo $file | xargs cat| grep -v ^# | while read i;do  
-    redis-cli -h $redis -p $redis_port -a $redis_pw -c  set $i  $(basename ${file} .netset); done
+    echo $file | xargs cat| grep -v ^# | while read i;do
+
+    if [[ ! "${whitelistarr[@]}"  =~ "$i" ]]; then
+       redis-cli -h $redis -p $redis_port -a $redis_pw -c  set $i  $(basename ${file} .netset)
+    fi
+
+   done
 fi
 
 
 done
-
-
